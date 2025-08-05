2c4aa6008c643e5624417f91b266f021e29c4a70
Marcelo <marcelopalmeira2004@hotmail.com>
2025-08-05 12:52:49 -0300

    melhorias

--- cg.c
+++ cg.c
@@ -1,19 +1,16 @@
 #include <GL/glut.h> 
 
 void initPicole(void){
-
 	glClearColor(0.0, 0.0, 0.0, 0.0);
-
 	gluOrtho2D(0.0, 500.0, 0, 500.0);
-
 }
 
-void desenhaPetala(float r, float g, float b){
+void desenhaPetalaDireta(int x0, int y0, int x1, int y1, int x2, int y2, float r, float g, float b) {
 	glColor3f(r, g, b);
 	glBegin(GL_POLYGON);
-		glVertex2i(250, 250);
-		glVertex2i(230, 200);
-		glVertex2i(270, 200);
+		glVertex2i(x0, y0);
+		glVertex2i(x1, y1);
+		glVertex2i(x2, y2);
 	glEnd();
 }
 
@@ -21,33 +18,21 @@ void picoleFunc(void){
 
 	glClear(GL_COLOR_BUFFER_BIT);
 
-	glColor3f(1.0, .0, .0);
+	glColor3f(1.0, 0.0, 0.0);
 	glBegin(GL_POLYGON);
 		glVertex2i(248, 250);
 		glVertex2i(251, 250);
+		glVertex2i(251, 100);
 		glVertex2i(248, 100);
-		glVertex2i(251,100);
 	glEnd();	
 
-	for(int i = 0; i < 4; i++)
-	{
-		float angula = i * 90.0f;
-		glPushMatrix();
-		glTranslatef(250.0f, 250.0f, 0.0f);
-		glRotatef(angula, 0.0f, 0.0f, 1.0f);
-		glTranslatef(-250.0f, -250.0f, 0.0f);
+	desenhaPetalaDireta(250, 250, 230, 200, 270, 200, 1.0, 0.0, 0.0);
 
-		// Cores do catavento: vermelho, amarelo, verde, azul
-		float r = 0.0f, g = 0.0f, b = 0.0f;
-		if (i == 0) { r = 1.0f; g = 0.0f; b = 0.0f; }      // vermelho
-		if (i == 1) { r = 1.0f; g = 1.0f; b = 0.0f; }      // amarelo
-		if (i == 2) { r = 0.0f; g = 1.0f; b = 0.0f; }      // verde
-		if (i == 3) { r = 0.0f; g = 0.0f; b = 1.0f; }      // azul
+	desenhaPetalaDireta(250, 250, 300, 230, 300, 270, 1.0, 1.0, 0.0);
 
-		desenhaPetala(r, g, b);
-	
-		glPopMatrix();
-	}
+	desenhaPetalaDireta(250, 250, 230, 300, 270, 300, 0.0, 1.0, 0.0);
+
+	desenhaPetalaDireta(250, 250, 200, 230, 200, 270, 0.0, 0.0, 1.0);
 
 	glFlush();
 }
@@ -55,16 +40,12 @@ void picoleFunc(void){
 int main(int argc, char** argv) {
 
 	glutInit(&argc, argv);
-
 	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
 	glutInitWindowSize(500, 500);
 	glutInitWindowPosition(200, 200);
-	glutCreateWindow("Picole");
+	glutCreateWindow("Picolé");
 
 	initPicole();
-
 	glutDisplayFunc(picoleFunc);
-
-
 	glutMainLoop();	
 }
